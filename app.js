51db60f25f48c6e739bbbb97774c3f2e2342ca2f
Saydullayev017 <saydullayevjavlonbek2002@gmail.com>
2024-12-20 18:02:34 +0500

    Обявил все нужные переменные в файле app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('suiiiiiii')
+let item = document.getElementsByClassName('game-item');
+let reset = document.getElementById('reset-game');
+let message = document.getElementById('message');
+
+let player = 'X';
+let stepCount = 0;
+let winCombinations = [
+    [1, 2, 3],
+    [1, 4, 7],
+    [1, 5, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let dataX = [];
+let dataO = [];
